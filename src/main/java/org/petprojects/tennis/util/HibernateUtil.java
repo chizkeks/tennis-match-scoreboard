11cc5ef9419df70dcde602bf04b384f24b402989
viktor_k <koloskov.vik@gmail.com>
2024-10-06 13:31:38 +0200

    Fix session

--- src/main/java/org/petprojects/tennis/util/HibernateUtil.java
+++ src/main/java/org/petprojects/tennis/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package org.petprojects.tennis.util;
 
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,14 +8,6 @@ import org.hibernate.cfg.Configuration;
 @UtilityClass
 public class HibernateUtil {
     Configuration config = new Configuration().configure();
-    public static SessionFactory getSessionFactory() {
-        return config.buildSessionFactory();
-    }
-
-    /*public static Session getSessionMultithreading() {
-            return (Session) Proxy.newProxyInstance(SessionFactory.class.getClassLoader(),
-                    new Class[]{Session.class},
-                    ((proxy, method, args) -> method.invoke(factory.getCurrentSession(), args)));
-
-    }*/
+    @Getter
+    private static final SessionFactory sessionFactory = config.buildSessionFactory();
 }
