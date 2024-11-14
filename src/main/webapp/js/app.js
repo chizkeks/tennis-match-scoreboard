1f0299f2a7d2a0f716ad09dd627757bcbfe95bd5
Ilya <pavlov.ilya.y@gmail.com>
2024-11-15 00:56:56 +0300

    UI refactoring: add main menu page

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,23 +1,3 @@
-function playerScored(scorer) {
-    let url = "ongoing-match" + window.location.search
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            scorer: scorer
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    }).then(response => {
-        // HTTP 301 response
-        // HOW CAN I FOLLOW THE HTTP REDIRECT RESPONSE?
-        if (response.redirected) {
-            window.location.href = response.url;
-        }
-    });
-    //location.reload()
-}
-
 // JavaScript for toggling the dropdown menu
 document.addEventListener("DOMContentLoaded", function () {
     const navToggle = document.querySelector(".nav-toggle");
